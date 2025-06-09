16b9de3fea53cb27dbd3d56138b13cde59af7a24
Erika Alban <erikalban12@gmail.com>
2025-06-09 14:55:10 -0400

    updates

--- upload.sh
+++ upload.sh
@@ -2,5 +2,16 @@
 
 for file in $(ls assets/*); do
     filename=$(basename $file)
-    aws s3 cp $file s3://erikaalban.com/${filename}
+    
+    # Set cache control headers based on file type
+    if [[ $filename == *.css ]] || [[ $filename == *.js ]]; then
+        # CSS and JS files - short cache with must-revalidate
+        aws s3 cp $file s3://erikaalban.com/${filename} --cache-control "max-age=300, must-revalidate"
+    elif [[ $filename == *.html ]]; then
+        # HTML files - no cache
+        aws s3 cp $file s3://erikaalban.com/${filename} --cache-control "no-cache, no-store, must-revalidate"
+    else
+        # Images and other assets - longer cache
+        aws s3 cp $file s3://erikaalban.com/${filename} --cache-control "max-age=86400"
+    fi
 done
